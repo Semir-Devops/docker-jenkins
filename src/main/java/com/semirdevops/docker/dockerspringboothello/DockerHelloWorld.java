3bf62c8877b4af3697e4dbcf173dc1c0d0f928c3
Semir-Devops <144611511+Semir-Devops@users.noreply.github.com>
2023-10-06 18:12:52 -0400

    Update DockerHelloWorld.java

--- src/main/java/com/semirdevops/docker/dockerspringboothello/DockerHelloWorld.java
+++ src/main/java/com/semirdevops/docker/dockerspringboothello/DockerHelloWorld.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/docker-container")
+@PageTitle("home")
 public class DockerHelloWorld {
 
 	@GetMapping
